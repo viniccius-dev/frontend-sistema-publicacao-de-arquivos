0951e945656593cc273f1fe8e24dabc92c614613
Vinícius <santsmarcos81@gmail.com>
2024-10-31 12:22:35 -0300

    Adicionado arquivo de rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import theme from "./styles/theme";
+import GlobalStyles from "./styles/global";
+import { AppRoutes } from "./routes";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <AppRoutes />
+    </ThemeProvider>
+  </React.StrictMode>
 )
---
+++ src/routes/index.jsx
@@ -0,0 +1,5 @@
+export function AppRoutes() {
+    return (
+        <h1>Olá</h1>
+    );
+}
